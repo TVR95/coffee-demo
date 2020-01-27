dd21063be93f0087208a49e3030041991cb1b140
TVR95 <kamil.szczepanski1@gmail.com>
2020-01-27 13:32:51 +0100

    Updates

---
+++ precache-manifest.28b746ac67615339f3c1a9e20ef225b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe51eef4740a859abd150199026f763",
+    "url": "/coffee-demo/index.html"
+  },
+  {
+    "revision": "dcc54309859cc7f20b98",
+    "url": "/coffee-demo/static/css/main.daab033d.chunk.css"
+  },
+  {
+    "revision": "358c59c531c688029d2a",
+    "url": "/coffee-demo/static/js/2.f6978b4c.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/coffee-demo/static/js/2.f6978b4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcc54309859cc7f20b98",
+    "url": "/coffee-demo/static/js/main.b8fc9e5f.chunk.js"
+  },
+  {
+    "revision": "6c3c914a272f395018b0",
+    "url": "/coffee-demo/static/js/runtime-main.8019b7e5.js"
+  },
+  {
+    "revision": "3daf4d418629bce7c7d794526d881d78",
+    "url": "/coffee-demo/static/media/caffee-about.3daf4d41.jpg"
+  }
+]);
